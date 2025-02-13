730732e053928b740234b0725d8def98467b29a1
Julius Kammerl <julius@kammerl.de>
2025-02-13 22:56:03 +0100

    final

--- seed/Rumpeln/main.cpp
+++ seed/Rumpeln/main.cpp
@@ -21,19 +21,27 @@ DaisySeed hw;
 TimerHandle timer;
 Switches switches;
 
-constexpr int kRecLenSamples = static_cast<int>(96000 * 40.f);
+constexpr int kRecLenSamples = static_cast<int>(96000 * 20.f);
 constexpr int kNumRecorder = 3;
 
-Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorder0;
-Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorder1;
-Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorder2;
-std::array<Recorder<float, kRecLenSamples>*, kNumRecorder> g_recorder = {
-    &g_recorder0, &g_recorder1, &g_recorder2};
+Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorderl0;
+Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorderr0;
+Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorderl1;
+Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorderr1;
+Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorderl2;
+Recorder<float, kRecLenSamples> DSY_SDRAM_BSS g_recorderr2;
+std::array<Recorder<float, kRecLenSamples>*, kNumRecorder> g_recorder_l = {
+    &g_recorderl0, &g_recorderl1, &g_recorderl2};
+std::array<Recorder<float, kRecLenSamples>*, kNumRecorder> g_recorder_r = {
+    &g_recorderr0, &g_recorderr1, &g_recorderr2};
 
 Limiter limiter_left;
 Limiter limiter_right;
 
 FilteredAdc fadc;
+
+Oscillator test_sound;
+float g_speed;
 
 float LinToLog(float input) {
   constexpr const float m = 0.1f;
@@ -49,36 +57,52 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
 
   for (int i = 0; i < kNumRecorder; ++i) {
     if (switches.Clicked(i)) {
-      g_recorder[i]->Reset();
+      g_recorder_l[i]->Reset();
+      g_recorder_r[i]->Reset();
     }
     if (switches.Clicked(i + kNumRecorder)) {
-      g_recorder[i]->Reset();
+      g_recorder_l[i]->Reset();
+      g_recorder_r[i]->Reset();
     }
   }
 
   for (size_t i = 0; i < size; i++) {
     float in_left = IN_L[i];
     float in_right = IN_R[i];
-    float mono = (in_left + in_right) / 2.0f;
+    // float mono = (in_left + in_right) / 2.0f;
 
-    float out_sample = 0.0f;
+    float out_sample_l = 0.0f;
+    float out_sample_r = 0.0f;
+
+    // float osc = test_sound.Process();
+    // in_left = osc;
+    // in_right = osc;
 
     for (int i = 0; i < kNumRecorder; ++i) {
       if (switches.Pressed(i)) {
-        g_recorder[i]->Write(mono);
+        g_recorder_l[i]->Write(in_left);
+        g_recorder_r[i]->Write(in_right);
+        // g_recorder[i]->Write(mono);
       } else {
-        const float volume = LinToLog(fadc.GetValue(i * 2 + 0));
-        const float speed = (fadc.GetValue(i * 2 + 1) - 0.5f) * 4.0f;
-        out_sample += g_recorder[i]->Read(speed) * volume;
+        const float volume = LinToLog(1.0f - fadc.GetValue(i * 2 + 0));
+        float speed = 0.0;
+        if (i == 2) {
+          speed = (1.0f - fadc.GetValue(i * 2 + 1) - 0.5f) * 6.0f;
+        } else {
+          speed = (1.0f - fadc.GetValue(i * 2 + 1) - 0.5f) * 3.0f;
+        }
+        g_speed = speed;
+        out_sample_l += g_recorder_l[i]->Read(speed) * volume;
+        out_sample_r += g_recorder_r[i]->Read(speed) * volume;
       }
     }
-    const float in_volume = LinToLog(fadc.GetValue(kNumRecorder * 2));
+    const float in_volume = LinToLog(1.0f - fadc.GetValue(kNumRecorder * 2));
 
-    OUT_L[i] = out_sample;
-    OUT_R[i] = out_sample;
+    OUT_L[i] = out_sample_l;
+    OUT_R[i] = out_sample_r;
 
-    OUT_L[i] += mono * in_volume;
-    OUT_R[i] += mono * in_volume;
+    OUT_L[i] += in_left * in_volume;
+    OUT_R[i] += in_right * in_volume;
   }
 
   limiter_left.ProcessBlock(OUT_L, size, /*pre_gain=*/1.0f);
@@ -96,7 +120,7 @@ void OutputStats() {
 
 int main(void) {
   hw.Init();
-  hw.SetAudioBlockSize(1);
+  hw.SetAudioBlockSize(64);
   hw.SetAudioSampleRate(daisy::SaiHandle::Config::SampleRate::SAI_96KHZ);
 
   TimerHandle::Config config;
@@ -110,8 +134,14 @@ int main(void) {
   fadc.Init(&hw);
   switches.Init(&hw, hw.AudioCallbackRate());
 
+  test_sound.Init(hw.AudioSampleRate());
+  test_sound.SetWaveform(Oscillator::WAVE_SIN);
+  // test_sound.SetFreq(440);
+  // test_sound.SetAmp(1.0);
+
   for (int i = 0; i < kNumRecorder; ++i) {
-    g_recorder[i]->Init();
+    g_recorder_l[i]->Init();
+    g_recorder_r[i]->Init();
   }
   limiter_left.Init();
   limiter_right.Init();
@@ -128,6 +158,7 @@ int main(void) {
     if (interval_sec > 1.f) {
       OutputStats();
       lastTime = newTick;
+      hw.PrintLine("g_speed: %d", (int)(g_speed * 10000.));
     }
 
     hw.DelayMs(1);
--- seed/Rumpeln/recorder.h
+++ seed/Rumpeln/recorder.h
@@ -26,54 +26,72 @@ class Recorder {
   /** clears buffer, sets write ptr to 0, and delay to 1 sample.
    */
   void Reset() {
-    start_ptr_ = 0;
-    len_samples_ = 0;
     write_ptr_ = 0;
     read_ptr_ = 0.0f;
+    is_recording_ = false;
   }
 
-  bool HasRecording() const { return len_samples_ > 0; }
+  bool HasRecording() const { return write_ptr_ > 0; }
 
   /** writes the sample of type T to the delay line, and advances the write ptr
    */
   inline void Write(const T sample) {
+    if (write_ptr_ == max_size) {
+      return;
+    }
+    is_recording_ = true;
     line_[write_ptr_] = sample;
     ++write_ptr_;
-    write_ptr_ = write_ptr_ % max_size;
+  }
 
-    if (len_samples_ < max_size) {
-      ++len_samples_;
-    } else {
-      start_ptr_ = write_ptr_;
+  static constexpr int kFadeWinNumSamples = 1024;
+
+  inline void Finish() {
+    const int fade_win_samples = std::min(kFadeWinNumSamples, write_ptr_ / 2);
+    if (fade_win_samples == 0) {
+      return;
     }
+    for (int i = 0; i < fade_win_samples; ++i) {
+      const float fade_in =
+          static_cast<float>(i) / static_cast<float>(fade_win_samples);
+      const float fade_out = 1.0f - fade_in;
+      line_[i] = line_[i] * fade_in +
+                 line_[write_ptr_ - fade_win_samples + i] * fade_out;
+    }
+    write_ptr_ -= fade_win_samples;
   }
 
   /** returns the next sample of type T in the delay line, interpolated if
    * necessary.
    */
-  inline const T Read(float speed) const {
-    if (len_samples_ == 0) {
+  inline const T Read(float speed) {
+    if (is_recording_) {
+      Finish();
+      is_recording_ = false;
+    }
+    if (write_ptr_ == 0) {
       return T(0);
     }
     const size_t read_idx = read_ptr_;
     const float fraction = fmod(read_ptr_, 1.0f);
-    const size_t read_pos = start_ptr_ + read_idx;
 
     read_ptr_ += speed;
-    if (read_ptr_ < 0) {
-      read_ptr_ += len_samples_;
+    while (read_ptr_ < 0) {
+      read_ptr_ += write_ptr_;
+    }
+    while (read_ptr_ >= write_ptr_) {
+      read_ptr_ -= write_ptr_;
     }
 
-    T a = line_[read_pos % len_samples_];
-    T b = line_[(read_pos + 1) % len_samples_];
+    T a = line_[read_idx % write_ptr_];
+    T b = line_[(read_idx + 1) % write_ptr_];
     return a + (b - a) * fraction;
   }
 
  private:
-  int start_ptr_;
-  int len_samples_;
   int write_ptr_;
-  mutable float read_ptr_;
+  mutable double read_ptr_;
+  bool is_recording_;
   T line_[max_size];
 };
 
